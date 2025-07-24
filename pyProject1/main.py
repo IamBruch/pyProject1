660339e06be7800a19594bee4dccf8b9f1d3e23f
IamBruch <simonrebidas@gmail.com>
2025-07-24 03:15:51 +0200

    Projekt sa bude tykat sachu -chesBoard done -ziskanie coordinacii policka done
    figurky
    skore

--- pyProject1/main.py
+++ pyProject1/main.py
@@ -1,22 +1,29 @@
 from tkinter import *
 import math
 
-
+ROWS = 8
+COLS = 20
 
 root = Tk()
 
-C = Canvas(root, height = 400, width = 400, bg = "#fbf5de")
+C = Canvas(root, height = ROWS * 50 + 100, width = COLS * 50, bg = "#fbf5de")
+chessBoard = [[0 for _ in range(COLS)] for _ in range(ROWS)]
+print(chessBoard)
 
 def drawChessBoard():
-    bSquareX = 50
-    bSquareY = 0
 
-    for i in range(8):
-        for i in range(4):
-            blackSquare = C.create_rectangle(bSquareX, bSquareY, bSquareX + 50, bSquareY + 50, fill="#660000")
-            bSquareX += 100
-        bSquareY += 50
-        bSquareX = 50 if bSquareY % 100 == 0 else 0
+    #Vykresli chessboard na canvas podla definovanych rozmerov - ROWS, COLS
+    #teda je flexibilny a da sa menit pocet riadkov a stlpcov
+
+    for row in range(ROWS):
+        for col in range(COLS):
+            x1 = col * 50
+            y1 = row * 50
+            x2 = x1 + 50
+            y2 = y1 + 50
+
+            color = "#fbf5de" if (row + col) % 2 == 0 else "#660000"
+            C.create_rectangle(x1, y1, x2, y2, fill=color)
 
 def convertCord(x, y):
     x = x / 50
@@ -30,7 +37,26 @@ def getCord(event):
     x,y = convertCord(event.x, event.y)
     print(f"Clicked at: x = {x}, y = {y}")
 
+
+
+
+class Piece:
+    def __init__(self, color, row, col):
+        self.row = int(row)
+        self.col = int(col)
+        self.color = color
+
+    def draw(self):
+        x1 = self.col * 50 + 5
+        y1 = self.row * 50 + 5
+        x2 = x1 + 25
+        y2 = y1 + 25
+        C.create_oval(x1, y1, x2, y2, fill=self.color)
+        C.create_polygon(x1 + 12.5, y1 + 25, x1 + 25, y1 + 12.5, x1 + 25, y1 + 25, fill=self.color)
+
+
 drawChessBoard()
+Piece("red", 0, 0).draw()
 # Example pieces
 
 root.bind("<Button-1>", getCord)
